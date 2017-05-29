09e1b91f9c7f57f306518e22d3d9b917fa92fa8f
ascratech2 <ascratech2@ascratech2s-Mac-mini.local>
2017-05-29 17:05:28 +0530

    Initial Commit

---
+++ sample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  sample
+//
+//  Created by ascratech2 on 29/05/17.
+//  Copyright © 2017 AscraTech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ sample/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  sample
+//
+//  Created by ascratech2 on 29/05/17.
+//  Copyright © 2017 AscraTech. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
